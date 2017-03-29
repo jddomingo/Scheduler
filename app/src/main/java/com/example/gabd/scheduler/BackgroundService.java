6b30a873dfd3bf36a29bcae83300802950a59544
jddomingo <gab.domingo123@gmail.com>
2017-03-30 00:59:10 +0800

    Alarm Database done.

--- app/src/main/java/com/example/gabd/scheduler/BackgroundService.java
+++ app/src/main/java/com/example/gabd/scheduler/BackgroundService.java
@@ -9,11 +9,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.SystemClock;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TimePicker;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by gabd on 2/13/17.
@@ -26,6 +29,7 @@ public class BackgroundService extends IntentService {
     private static SchedulePage inst;
     private AlarmReceiver alarm;
     private Context context;
+    TinyDB tdb;
     public static final String MyPREFERENCES = "alarmspref";
     SharedPreferences sharedpreferences;
     /**
@@ -63,20 +67,28 @@ public class BackgroundService extends IntentService {
 
         alarm_manager = (AlarmManager) getSystemService(ALARM_SERVICE);
         final Calendar calendar = Calendar.getInstance();
+        TinyDB tdb = new TinyDB(this);
+        String idlist = "idlist";
+        String hourlist = "hourlist";
+        String minutelist = "minutelist";
+        String actnames = "actnames";
+        ArrayList<Integer> listid = tdb.getListInt(idlist);
+        ArrayList<Integer> listhour = tdb.getListInt(hourlist);
+        ArrayList<Integer> listmin = tdb.getListInt(minutelist);
+        ArrayList<String> activitynames = tdb.getListString(actnames);
+
 
         Intent myIntent = new Intent(this, AlarmReceiver.class);
         myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
         String str_min = intent.getStringExtra("MINUTE");
-        Log.e("BackgroundService", ""+intent.getStringExtra("HOUR"));
         String str_hour = intent.getStringExtra("HOUR");
+        String act_name = intent.getStringExtra("NAME");
+        Log.e("BackgroundService", ""+intent.getStringExtra("NAME"));
 
         int minute;
         minute = Integer.parseInt(str_min);
         int hour = Integer.parseInt(str_hour);
-
-        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedpreferences.edit();
 
         calendar.set(Calendar.HOUR_OF_DAY, hour);
         calendar.set(Calendar.MINUTE, minute);
@@ -85,15 +97,19 @@ public class BackgroundService extends IntentService {
         if (minute < 10) str_min = "0" + String.valueOf(minute);
         if (hour > 12) str_hour = String.valueOf(hour - 12);
 
-
-
         //editor = sharedPref.edit();
         final int _id = (int)calendar.getTimeInMillis();
-        editor.putInt("id", _id);
-        editor.commit();
+        listid.add(_id);
+        listhour.add(hour);
+        listmin.add(minute);
+        activitynames.add(act_name);
+        tdb.putListInt(idlist, listid);
+        tdb.putListInt(hourlist, listhour);
+        tdb.putListInt(minutelist, listmin);
+        tdb.putListString(actnames, activitynames);
 
         pending_intent = PendingIntent.getBroadcast(BackgroundService.this, _id, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        alarm_manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 1000*40, pending_intent);
+        alarm_manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pending_intent);
     }
 }

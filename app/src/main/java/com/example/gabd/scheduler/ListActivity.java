649e65fbbc785b4c7ea5b2826a8ccd8cc4ca8f80
jddomingo <gab.domingo123@gmail.com>
2017-04-03 23:26:48 +0800

    Added delete feature.

--- app/src/main/java/com/example/gabd/scheduler/ListActivity.java
+++ app/src/main/java/com/example/gabd/scheduler/ListActivity.java
@@ -1,7 +1,10 @@
 package com.example.gabd.scheduler;
 import com.example.gabd.scheduler.Alarm;
+import com.github.brnunes.swipeablerecyclerview.SwipeableRecyclerViewTouchListener;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -21,6 +24,9 @@ public class ListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        tdb = new TinyDB(this);
+        Intent myIntent = new Intent(this, AlarmReceiver.class);
+        myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
         recView = (RecyclerView) findViewById(R.id.recview);
 
@@ -29,12 +35,11 @@ public class ListActivity extends Activity {
 
         initializeData();
         initializeAdapter();
-
     }
+
 
     private void initializeData(){
         alarms = new ArrayList<>();
-        tdb = new TinyDB(this);
         ArrayList<Object> alarmlist = tdb.getListObject("alarmlist", Alarm.class);
         for (int i = 0; i < alarmlist.size(); i++) {
             alarms.add((Alarm) alarmlist.get(i));
@@ -42,7 +47,60 @@ public class ListActivity extends Activity {
     }
 
     private void initializeAdapter(){
-        MyAdapter adapter = new MyAdapter(alarms);
+        final MyAdapter adapter = new MyAdapter(alarms);
         recView.setAdapter(adapter);
+
+        SwipeableRecyclerViewTouchListener swipeTouchListener =
+                new SwipeableRecyclerViewTouchListener(recView,
+                        new SwipeableRecyclerViewTouchListener.SwipeListener() {
+                            PendingIntent pending_intent;
+                            ArrayList<Object> alarmlist = tdb.getListObject("alarmlist", Alarm.class);
+                            @Override
+                            public boolean canSwipe(int position) {
+                                return true;
+                            }
+
+                            @Override
+                            public boolean canSwipeLeft(int position) {
+                                return true;
+                            }
+
+                            @Override
+                            public boolean canSwipeRight(int position) {
+                                return true;
+                            }
+
+                            @Override
+                            public void onDismissedBySwipeLeft(RecyclerView recyclerView, int[] reverseSortedPositions) {
+                                for (int position : reverseSortedPositions) {
+                                    alarms.remove(position);
+                                    Alarm delete_alarm = (Alarm) alarmlist.get(position);
+                                    Intent myIntent = new Intent(recView.getContext(), AlarmReceiver.class);
+                                    myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    pending_intent = PendingIntent.getBroadcast(recView.getContext(), delete_alarm.getId(), myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                                    alarmlist.remove(position);
+                                    tdb.putListObject("alarmlist", alarmlist);
+                                    adapter.notifyItemRemoved(position);
+                                }
+                                adapter.notifyDataSetChanged();
+                            }
+
+                            @Override
+                            public void onDismissedBySwipeRight(RecyclerView recyclerView, int[] reverseSortedPositions) {
+                                for (int position : reverseSortedPositions) {
+                                    alarms.remove(position);
+                                    Alarm delete_alarm = (Alarm) alarmlist.get(position);
+                                    Intent myIntent = new Intent(recView.getContext(), AlarmReceiver.class);
+                                    myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    pending_intent = PendingIntent.getBroadcast(recView.getContext(), delete_alarm.getId(), myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                                    alarmlist.remove(position);
+                                    tdb.putListObject("alarmlist", alarmlist);
+                                    adapter.notifyItemRemoved(position);
+                                }
+                                adapter.notifyDataSetChanged();
+                            }
+                        });
+        recView.addOnItemTouchListener(swipeTouchListener);
     }
+
 }
